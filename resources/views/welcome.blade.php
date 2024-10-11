15e6feef67f206892eb95a73acd8576854dfc3fb
keampichvireak <82803740+keampichvireak@users.noreply.github.com>
2024-10-11 17:50:33 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sbay Tinh</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+
+    <!-- Fixed Header -->
+    <div class="position-fixed top-0 start-0 w-100">
+        <div class="p-3 mb-2 text-bg-dark d-flex align-items-center justify-content-between">
+            <div class="d-flex"> 
+                <img src="https://i.pinimg.com/564x/54/68/a3/5468a33bc43e258995f15ad01e5ad7cc.jpg" alt="gaming controller" style="width: 60px; height: auto;">
+            <h4 class="ms-3">Sbay Tinh</h4>
+            </div>
+        </div>
+    </div>
+
+    <!-- Scrollable Content in the middle -->
+    <div class="container mt-5 pt-5 text-center">
+        <div class="row">
+          <div class="col">
+            <img src="https://i.pinimg.com/564x/82/55/03/8255033248d018b6c5f3d460b2deec16.jpg" alt="" style="width: 100px; height: auto;">
+            <p>Mobile Legends</p>
+          </div>
+          <div class="col">
+            <img src="https://i.pinimg.com/564x/51/6a/74/516a74d6d701c86c007f668d7cf2891a.jpg" alt="" style="width: 100px; height: auto;">
+            <p>PubG</p>
+          </div>
+          <div class="col">
+            <img src="https://i.pinimg.com/564x/98/92/8c/98928c82f78bdd50c5a28d8fa4633035.jpg" alt="" style="width: 100px; height: auto;">
+            <p>Clah of Clans</p>
+          </div>
+          <div class="col">
+            <img src="https://i.pinimg.com/564x/c6/70/11/c6701170a4247471beb3c8602c5bfe88.jpg" alt="" style="width: 100px; height: auto;">
+            <p>Vainglory</p>
+          </div>
+          <div class="col">
+            <img src="https://i.pinimg.com/564x/69/81/7e/69817eac50792cffdb7973074e4b5bde.jpg" alt="" style="width: 100px; height: auto;">
+            <p>Candy Crush</p>
+          </div>
+          <div class="col">
+            <img src="https://i.pinimg.com/564x/a7/2f/a4/a72fa4dea05328a0335e166a0d9646af.jpg" alt="" style="width: 100px; height: auto;">
+            <p>Zombie Tsunami</p>
+          </div>
+          <div class="col">
+            <img src="https://i.pinimg.com/564x/55/cc/47/55cc47dffb86808e594877078656404b.jpg" alt="" style="width: 100px; height: auto;">
+            <p>Temple Run</p>
+          </div>
+          <div class="col">
+            <img src="https://i.pinimg.com/564x/82/55/03/8255033248d018b6c5f3d460b2deec16.jpg" alt="" style="width: 100px; height: auto;">
+            <p>Mobile Legends</p>
+          </div>
+          <div class="col">
+            <img src="https://i.pinimg.com/564x/51/6a/74/516a74d6d701c86c007f668d7cf2891a.jpg" alt="" style="width: 100px; height: auto;">
+            <p>PubG</p>
+          </div>
+          <div class="col">
+            <img src="https://i.pinimg.com/564x/98/92/8c/98928c82f78bdd50c5a28d8fa4633035.jpg" alt="" style="width: 100px; height: auto;">
+            <p>Clah of Clans</p>
+          </div>
+          <div class="col">
+            <img src="https://i.pinimg.com/564x/c6/70/11/c6701170a4247471beb3c8602c5bfe88.jpg" alt="" style="width: 100px; height: auto;">
+            <p>Vainglory</p>
+          </div>
+          <div class="col">
+            <img src="https://i.pinimg.com/564x/69/81/7e/69817eac50792cffdb7973074e4b5bde.jpg" alt="" style="width: 100px; height: auto;">
+            <p>Candy Crush</p>
+          </div>
+          <div class="col">
+            <img src="https://i.pinimg.com/564x/a7/2f/a4/a72fa4dea05328a0335e166a0d9646af.jpg" alt="" style="width: 100px; height: auto;">
+            <p>Zombie Tsunami</p>
+          </div>
+          <div class="col">
+            <img src="https://i.pinimg.com/564x/55/cc/47/55cc47dffb86808e594877078656404b.jpg" alt="" style="width: 100px; height: auto;">
+            <p>Temple Run</p>
+          </div>
+        </div>
+        <form>
+            <input type="checkbox" id="checkbox1" name="option1">
+            <label for="checkbox1"><p style="color: red;">Term & Condition:</p></label>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque totam modi, magni, ea quasi nemo reprehenderit eveniet quidem ipsa est deleniti consequatur distinctio repellat amet porro quo, aspernatur exercitationem? Vitae.</p>
+          </form>
+    </div>
+    <div class="d-flex flex-column justify-content-center align-items-center vh-10" style="margin-bottom: 70px;">
+        <div class="text-center">
+            <img src="https://i.pinimg.com/564x/b0/ea/3c/b0ea3caabdf2df575136add73392d671.jpg" alt="ABA Bank" style="width: 200px; height: auto;">
+            <p>ABA Bank</p>
+        </div>
+        <div class="mt-3">
+            <button type="button" class="btn btn-success" style="width:200px; height:40px; font-size: 17px;">Pay here</button>
+        </div>
+    </div>
+
+
+    <!-- Fixed Footer (Progress Bar and Button) -->
+    <div class="position-fixed bottom-0 w-100">
+        
+        <div class="position-relative m-4 text-bg-dark">
+            <div class="progress" role="progressbar" aria-label="Progress" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="height: 1px;">
+                <div class="progress-bar" style="width: 50%"></div>
+            </div>
+            <button type="button" class="position-absolute top-0 start-0 translate-middle btn btn-sm btn-primary rounded-pill" style="width: 2rem; height:2rem;">1</button>
+            <button type="button" class="position-absolute top-0 start-50 translate-middle btn btn-sm btn-primary rounded-pill" style="width: 2rem; height:2rem;">2</button>
+            <button type="button" class="position-absolute top-0 start-100 translate-middle btn btn-sm btn-secondary rounded-pill" style="width: 2rem; height:2rem;">3</button>
+        </div>
+    </div>
+    
+</body>
+</html>
